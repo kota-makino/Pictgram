f50e27b830a6aaafe83cc92b3f7446195f6b76b5
kota-makino <maxfafafa@gmail.com>
2019-01-04 21:05:14 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+
+  has_secure_password
+
+  validates :password, length: { in: 8..32 }
+  validates :password_confirmation, length: { in: 8..32 }
+
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :password_confirmation, format: { with: /\A[a-zA-Z0-9]+\z/ }
+
+  validates :email, format: { with: /\A\A[\w+\-.]+@[a-z\d|\-.]+\.[a-z]+\z/i }
+
+  validates :name, length: { maximum: 15 }
+end
