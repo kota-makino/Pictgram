7dd8ff4e2d009b046d7a59379230eade098382f6
kota-makino <maxfafafa@gmail.com>
2019-01-15 06:16:04 +0900

    kadai commit r6

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   root 'pages#index'
   get 'pages/help'
 
-  resources :users # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
